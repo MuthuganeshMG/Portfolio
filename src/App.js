7115399293e60493640b5c9cb6e84adb4a7d96dc
MuthuganeshMG <muthuganesh3597@gmail.com>
2024-08-01 10:36:53 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-import TopSection from './components/TopSection';
-import Footer from './components/Footer';
 import Header from './components/Header';
+import TopSection from './components/TopSection';
 import Skill from './components/Skill';
 import ProjectContainer from './components/ProjectContainer';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <TopSection/>
-      <Skill/>
-      <ProjectContainer/>
-      <Footer/>
+      <Header />
+      <TopSection />
+      <Skill />
+      <ProjectContainer />
+      <Footer />
     </div>
   );
 }
