d4a2e67313d9f4512e60a47f9099be2c92ca27a5
MuthuganeshMG <muthuganesh3597@gmail.com>
2024-08-04 14:19:40 +0530

    skill style changes

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,29 +1,41 @@
 import React from 'react';
-import './Skill.css';
+import './skill.css';
 import skillset from '../assets/skillset.jpg';
 
 export default function Skill() {
     return (
         <section className='skill-container' id='skill'>
-            <h2>SkillS</h2>
-            <div className="skill-img">
-                <img src={skillset} alt="Skillset" />
+            <div className="skill-header">
+                <h2 className='skill-head'>SkillS</h2>
             </div>
-            <div className="right">
-                <h3 className='right-head'>Back-End</h3>
-                <div className="skillset-right" >
-                    <h5>Node Js</h5>
-                    <h5>Express JS</h5>
-                    <h5>Mongo DB</h5>
+            <div className="skill-image">
+                <img src={skillset} alt="Skillset" className='skill-img' />
+            </div>
+            <div className="skill-section">
+                <div className="right-container">
+                    <div className="right">
+                        <h3 className='right-head'>
+                            Back-End
+                        </h3>
+                        <div className="right-skillset" >
+                            <h5>Mongo DB</h5>
+                            <h5>Express JS</h5>
+                            <h5>Node Js</h5>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className="left">
-                <h3 className='left-head'>Front-End</h3>
-                <div className="skillset-left" >
-                    <h5>React</h5>
-                    <h5>JavaScript</h5>
-                    <h5>Css3</h5>
-                    <h5>Html5</h5>
+                <div className="left-container">
+                    <div className="left">
+                        <h3 className='left-head'>
+                            Front-End
+                        </h3>
+                        <div className="left-skillset" >
+                            <h5>React</h5>
+                            <h5>JavaScript</h5>
+                            <h5>Css3</h5>
+                            <h5>Html5</h5>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
